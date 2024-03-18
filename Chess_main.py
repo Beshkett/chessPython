30704e45c97cbaaa925ea1a2ff88af6cd8d09605
YukeryStudent <116521611+YukeryStudent@users.noreply.github.com>
2024-03-18 08:47:27 +0100

    Refactor piece rendering and possible move visualization

--- Chess_main.py
+++ Chess_main.py
@@ -2,10 +2,6 @@ import pygame
 import chess
 import chess.engine
 
-import pygame
-import chess
-import chess.engine
-
 class ChessGame:
     """
     Represents a game of chess.
@@ -79,7 +75,10 @@ class ChessGame:
             if piece is not None:
                 row, col = 7 - square // 8, square % 8
                 image = self.piece_images[piece.piece_type, piece.color]
-                self.screen.blit(image, pygame.Rect(col * self.square_size, row * self.square_size, self.square_size, self.square_size))
+                image_width, image_height = image.get_size()
+                x = col * self.square_size + self.square_size // 2 - image_width // 2
+                y = row * self.square_size + self.square_size // 2 - image_height // 2
+                self.screen.blit(image, (x, y))
 
     def draw_possible_moves(self):
         """
@@ -89,7 +88,7 @@ class ChessGame:
             for move in self.board.legal_moves:
                 if move.from_square == self.selected_square:
                     row, col = 7 - move.to_square // 8, move.to_square % 8
-                    pygame.draw.circle(self.screen, (255, 0, 0), (col * self.square_size + self.square_size // 2, row * self.square_size + self.square_size // 2), 10)
+                    pygame.draw.circle(self.screen, (135, 24, 16), (col * self.square_size + self.square_size // 2, row * self.square_size + self.square_size // 2), 10)
 
     def play(self):
         """
